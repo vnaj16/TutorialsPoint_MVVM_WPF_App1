cfb3662aa0295534f92da9a7de8eb12a1d0ad1d8
Arthur <arthur1610@live.com>
2020-05-10 09:15:09 -0500

    Initial commit

---
+++ TutorialsPoint_MVVM_WPF_App1/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TutorialsPoint_MVVM_WPF_App1.ViewModel;
+
+namespace TutorialsPoint_MVVM_WPF_App1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void StudentViewControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            StudentViewModel studentViewModelObject = new StudentViewModel();
+            //studentViewModelObject.LoadStudents();
+
+            //StudentViewControl.DataContext = studentViewModelObject;
+        }
+    }
+}
---
+++ TutorialsPoint_MVVM_WPF_App1/ViewModel/StudentViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TutorialsPoint_MVVM_WPF_App1.Model;
+
+namespace TutorialsPoint_MVVM_WPF_App1.ViewModel
+{
+    class StudentViewModel
+    {
+        public StudentViewModel()
+        {
+            LoadStudents();
+        }
+
+        public ObservableCollection<Student> Students
+        {
+            get;
+            set;
+        }
+
+        public void LoadStudents()
+        {
+            ObservableCollection<Student> students = new ObservableCollection<Student>();
+
+            students.Add(new Student { FirstName = "Mark", LastName = "Allain" });
+            students.Add(new Student { FirstName = "Allen", LastName = "Brown" });
+            students.Add(new Student { FirstName = "Linda", LastName = "Hamerski" });
+
+            Students = students;
+        }
+    }
+}
